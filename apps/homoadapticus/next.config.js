6893f0c3bd10699d00d66aaffc31fac322091766
Amare <amareteklay@gmail.com>
2026-03-25 11:11:05 +0100

    fix: replace next config homoadapticus

--- apps/homoadapticus/next.config.js
+++ apps/homoadapticus/next.config.js
@@ -1,6 +1,11 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@amare-web/ui'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
